bf21324e604cfd5da1c91f187de9dfddf66ab0b1
Ronan <connollyro@gmail.com>
2019-03-03 14:16:00 +0000

    Check added to prevent user from doing multiple recordings without submitting/discarding it

--- src/js/lesson_helpers.js
+++ src/js/lesson_helpers.js
@@ -1,5 +1,3 @@
-import { removeChildNodes } from './index_helpers';
-
 /*
 Code for recording audio was modified from these sources/tutorials:
 https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
@@ -8,12 +6,11 @@ https://addpipe.com/blog/using-recorder-js-to-capture-wav-audio-in-your-html5-we
 let getUserMediaStream;
 let recorderObj;
 let mediaStreamAudioSourceNode;
-
 let recordAudioContext;
+let recordingPresent = false
 
 //TESTING
 let playbackAudioContext = new AudioContext();
-//const source = playbackAudioContext.createBufferSource();
 
 let globalAudioBuffer;
 
@@ -22,32 +19,14 @@ export function loadWavIntoBuffer(url){
     .then(response => response.arrayBuffer())
     .then(arrayBuffer => playbackAudioContext.decodeAudioData(arrayBuffer))
     .then(audioBuffer => {
-		//console.log("In loadWavIntoBuffer");
         return audioBuffer;
     })
     .then(audioBuffer =>{
-			//playWav(audioBuffer);
 			globalAudioBuffer = audioBuffer;
     });
 
 }
 
-// export function playLesson(e){
-//     console.log(e.target.innerText);
-//     console.log(e.target.myParam);
-//     let url = e.target.myParam;
-
-//     var playbackAudioContext = new AudioContext();
-//     fetch(url)    
-//     .then(response => response.arrayBuffer())
-//     .then(arrayBuffer => playbackAudioContext.decodeAudioData(arrayBuffer))
-//     .then(audioBuffer => {
-//         return audioBuffer;
-//     })
-//     .then(audioBuffer =>{
-//             playWav(audioBuffer,playbackAudioContext);
-//     });
-// }
 
 export function playLesson(e){
 	    console.log(e.target.innerText);
@@ -105,24 +84,31 @@ export function recordLesson(e){
 	let rec_btn = document.getElementById('record_button');
 	rec_btn.disabled = true;
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-		recordAudioContext = new AudioContext();
+	if(recordingPresent){
+		console.log("You have already recorded. Submit it or discard it");
+		alert("You have already recorded. Submit it or discard it");
+	}
+	else
+	{
+		navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+			console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+			recordAudioContext = new AudioContext();
 
-		let contextDetails = "Format: 2 channel pcm @ " + recordAudioContext.sampleRate/1000+"kHz";
-		console.log(contextDetails);
+			let contextDetails = "Format: 2 channel pcm @ " + recordAudioContext.sampleRate/1000+"kHz";
+			console.log(contextDetails);
 
-		getUserMediaStream = stream;
-		mediaStreamAudioSourceNode = recordAudioContext.createMediaStreamSource(stream);
-		//record audio in stereo
-		recorderObj = new Recorder(mediaStreamAudioSourceNode,{numChannels:2});
-		recorderObj.record();
-		console.log("Recording......");
+			getUserMediaStream = stream;
+			mediaStreamAudioSourceNode = recordAudioContext.createMediaStreamSource(stream);
+			//record audio in stereo
+			recorderObj = new Recorder(mediaStreamAudioSourceNode,{numChannels:2});
+			recorderObj.record();
+			console.log("Recording......");
+			
 
-	}).catch(error => {
-		console.log(error);
-	})
-
+		}).catch(error => {
+			console.log(error);
+		})
+	}
 }
 
 export function stopRecording(e){
@@ -137,6 +123,7 @@ export function stopRecording(e){
 
 	//Create wav blog to be posted to server for analysis
 	recorderObj.exportWAV(postWavToServer);
+	recordingPresent = true;
 
 	function postWavToServer(blob){
 		let audio_player_container = document.getElementById('audio_player_container');
@@ -193,6 +180,7 @@ export function stopRecording(e){
 				success: function (data) {
 					console.log(typeof data);
 					console.log(data);
+					recordingPresent = false;
 				}
 			});
 		}
@@ -210,6 +198,8 @@ export function stopRecording(e){
 
 			let discard_recording_button = document.getElementById('discard_recording_button');
 			discard_recording_button.remove();
+
+			recordingPresent = false;
 			
 		}
 	}// end postWavToServer() 
