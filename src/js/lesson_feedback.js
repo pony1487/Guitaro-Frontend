b242b1d1b898b5a2aba7000066860374288e2477
Ronan <connollyro@gmail.com>
2019-03-23 15:03:17 +0000

    Added basic test for overlaying notes using canvas

--- src/js/lesson_feedback.js
+++ src/js/lesson_feedback.js
@@ -9,6 +9,7 @@ export function processFeedbackJSON(feedBackObj){
 
     
     //draw user
+    test_canvas();
     draw_tab(user_string_list,user_fret_list,user_note_durations,user_total_beats,"user_notation");
     
 
@@ -28,6 +29,58 @@ export function processFeedbackJSON(feedBackObj){
     // timing_difference_p.textContent = "Timing Difference: " + feedBackObj.percentage_difference;
     // wrong_notes_played_p.textContent = "Wrong Notes Played: " + feedBackObj.notes_not_in_lesson;
 
+}
+
+function test_canvas(){
+    let num_of_notes = 6;
+
+    //These were eyeballed as the Tab stave from vexflow starts in the middle of the canvas somewhere
+    let y_pos_of_high_e = 92;
+    let y_pos_of_low_e = 157;
+
+    // the width of the stave is 450.
+    let width = 450;
+    let height = y_pos_of_low_e - y_pos_of_high_e;
+ 
+
+    let x_position_diff = width / num_of_notes;
+    let string = height / 6;
+
+    let string_coordinates = {}
+
+    let start_string_coordinate = y_pos_of_high_e;
+    let string_number = 1;
+    for(let i =0;i < 6;i++){
+        string_coordinates[string_number] = start_string_coordinate;
+        start_string_coordinate += string;
+        string_number++;
+    }
+
+    console.log(string_coordinates);
+
+    let canvas = document.getElementById("userNotationCanvas");
+    let ctx = canvas.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.fillStyle = "red";
+
+    let x_pos = 45;
+    let y_pos_start = 150;
+
+    //ctx.fillRect(x_pos,157,15,15); 
+    //ctx.fillRect(x_pos,92,15,15); 
+
+
+
+    let test_string_nums = [6,6,5,5,4,4];
+
+    for(let i = 0; i < num_of_notes;i++){
+        let string_played = test_string_nums[i];
+        //console.log(string_played);
+        let y_pos = string_coordinates[string_played];
+        
+        ctx.fillRect(x_pos,y_pos,15,15); 
+        x_pos += x_position_diff;
+    }
 }
 
 //module.exports = processFeedbackJSON;
